ae22b310346427ec31630a705a473eda520b28d7
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-23 05:25:40 -0600

    Initial commit

---
+++ src/JavaRegexWordBoundaries.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaRegexWordBoundaries {
+
+  public static void main(String[] args) {
+    var text = "This island is beautiful";
+
+    Pattern p = Pattern.compile("\\bis\\b");
+
+    Matcher matcher = p.matcher(text);
+
+    while (matcher.find()) {
+      System.out.printf("%s at %d" , matcher.group(), matcher.start());
+    }
+  }
+}
